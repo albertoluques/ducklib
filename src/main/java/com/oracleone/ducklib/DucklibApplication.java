20825efeda35cc3db21bbcbd4611607062580a0e
AlbertoLuque <157401388+albertoluques@users.noreply.github.com>
2024-05-16 12:48:38 -0700

    first commit

---
+++ src/main/java/com/oracleone/ducklib/DucklibApplication.java
@@ -0,0 +1,13 @@
+package com.oracleone.ducklib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucklibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DucklibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oracleone/ducklib/DucklibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracleone.ducklib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DucklibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
