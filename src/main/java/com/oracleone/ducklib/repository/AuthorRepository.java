21d5940702460a9a0737b3a48b46c83b8f676052
albertoluques <157401388+albertoluques@users.noreply.github.com>
2024-06-25 09:49:26 -0700

    Add delivered Queries

--- src/main/java/com/oracleone/ducklib/repository/AuthorRepository.java
+++ src/main/java/com/oracleone/ducklib/repository/AuthorRepository.java
@@ -1,4 +1,15 @@
 package com.oracleone.ducklib.repository;
 
-public interface AuthorRepository {
+import com.oracleone.ducklib.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> findByName(String name);
+
+    @Query("SELECT a FROM Author a WHERE a.birthYear <= :year AND a.deathYear >= :year")
+    List<Author> authorsAliveByYear(Integer year);
 }
--- src/main/java/com/oracleone/ducklib/repository/BookRepository.java
+++ src/main/java/com/oracleone/ducklib/repository/BookRepository.java
@@ -1,4 +1,19 @@
 package com.oracleone.ducklib.repository;
 
-public class BookRepository {
+import com.oracleone.ducklib.model.Book;
+import com.oracleone.ducklib.model.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByLanguages(Language language);
+
+    Optional<Book> findByTitle(String title);
+
+    @Query("SELECT b FROM Book b ORDER BY b.downloads DESC LIMIT 10")
+    List<Book> top10DownloadedBooks();
 }
