8fd330834bb2d525d9fc7ff7e821ea555dc9afd2
albertoluques <157401388+albertoluques@users.noreply.github.com>
2024-06-25 09:51:44 -0700

    Migrate to MySQL

--- src/main/java/com/oracleone/ducklib/DucklibApplication.java
+++ src/main/java/com/oracleone/ducklib/DucklibApplication.java
@@ -2,22 +2,29 @@ package com.oracleone.ducklib;
 
 import com.oracleone.ducklib.model.BookData;
 import com.oracleone.ducklib.principal.Principal;
+import com.oracleone.ducklib.repository.AuthorRepository;
+import com.oracleone.ducklib.repository.BookRepository;
 import com.oracleone.ducklib.service.ConsumeAPI;
 import com.oracleone.ducklib.service.DataConversion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DucklibApplication implements CommandLineRunner {
+	@Autowired
+	private BookRepository bookRepository;
 
+	@Autowired
+	private AuthorRepository authorRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(DucklibApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
+		Principal principal = new Principal(bookRepository, authorRepository);
 		principal.showMenu();
 	}
 }
