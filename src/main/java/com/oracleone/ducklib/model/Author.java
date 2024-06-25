9395fa64ab8018cd1431b79d465ff95db12bbc46
albertoluques <157401388+albertoluques@users.noreply.github.com>
2024-06-25 09:45:00 -0700

    Change how code it's structured

--- src/main/java/com/oracleone/ducklib/model/Author.java
+++ src/main/java/com/oracleone/ducklib/model/Author.java
@@ -1,4 +1,86 @@
 package com.oracleone.ducklib.model;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Entity
+@Table(name = "authors")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer birthYear;
+    private Integer deathYear;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Book> book;
+
+    
+    public Author(){}
+
+    public Author(AuthorData authorData) {
+        this.name = authorData.name();
+        this.birthYear = authorData.birthYear();
+        this.deathYear = authorData.deathYear();
+    }
+
+
+
+    // getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public Integer getDeathYear() {
+        return deathYear;
+    }
+
+    public void setDeathYear(Integer deathYear) {
+        this.deathYear = deathYear;
+    }
+
+    public List<Book> getBook() {
+        return book;
+    }
+
+    public void setBook(List<Book> book) {
+        this.book = book;
+    }
+
+
+    // to String
+
+    @Override
+    public String toString() {
+        return
+                "name='" + name + '\'' +
+                ", birthYear=" + birthYear +
+                ", deathYear=" + deathYear +
+                ", book=" + book;
+    }
 }
--- src/main/java/com/oracleone/ducklib/model/AuthorData.java
+++ src/main/java/com/oracleone/ducklib/model/AuthorData.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Person(
+public record AuthorData(
+        @JsonAlias("name") String name,
         @JsonAlias("birth_year") Integer birthYear,
-        @JsonAlias("death_year") Integer deathYear,
-        @JsonAlias("name") String name
+        @JsonAlias("death_year") Integer deathYear
 ) {
 }
